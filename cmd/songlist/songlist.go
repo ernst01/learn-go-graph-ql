c827d3f9550c43d39f957ad4e54e6973967128e6
Thiebaud E <thiebaud.ernstberger@gmail.com>
2018-12-17 13:23:00 -0700

    Added GraphQL example for starting point

--- cmd/songlist/songlist.go
+++ cmd/songlist/songlist.go
@@ -1,15 +1,48 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/graphql-go/graphql"
 
 	"github.com/ernst01/learn-go-graph-ql/internal/songlist"
 )
 
 func main() {
+
+	// Schema
+	fields := graphql.Fields{
+		"hello": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "world", nil
+			},
+		},
+	}
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+	schema, err := graphql.NewSchema(schemaConfig)
+	if err != nil {
+		log.Fatalf("failed to create new schema, error: %v", err)
+	}
+
+	// Query
+	query := `
+		{
+			hello
+		}
+	`
+	params := graphql.Params{Schema: schema, RequestString: query}
+	r := graphql.Do(params)
+	if len(r.Errors) > 0 {
+		log.Fatalf("failed to execute graphql operation, errors: %+v", r.Errors)
+	}
+	rJSON, _ := json.Marshal(r)
+	fmt.Printf("%s \n", rJSON)
 
 	srv := songlist.Server{
 		Router: mux.NewRouter(),
